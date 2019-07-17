03edad0e613bbdd75be35b84fa5229aac68ad61a
Simion Cira <simion.cira@intern.arobs.com>
2019-07-17 10:58:30 +0300

    added reducer tests

--- src/App.test.js
+++ src/App.test.js
@@ -8,6 +8,8 @@ import * as serviceWorker from './serviceWorker';
 import {compose,applyMiddleware,createStore} from 'redux'
 import rootReducer from './reducers/index.js' //ToDo: creare reducer
 import stories from './reducers/stories';
+import tasks from './reducers/tasks';
+import { TaskStatus } from './actions';
 
 const composeEnhancers =
   typeof window === 'object' &&
@@ -26,11 +28,40 @@ test('test1',()=>{
   expect(1).toBe(1)
 })
 const story1={id:1,title:'story1',taskIds:[1]}
+
 test('addStory',()=>{
   expect(
     stories([story1],{type:'ADD_STORY',id:2,title:'story2'})
     ).toStrictEqual([{id:2,title:'story2',taskIds:[]},story1])
 })
+const addTaskActionTest={
+  type:'ADD_TASK',
+  id:2,
+  storyId:1,
+  task:{
+    title:'task2',
+    description:'taskul jmecher',
+    taskType:'Bug',
+    status:'NEW'
+  }
+}
+test('addTask, added Id to Story',()=>{
+  expect(stories([story1],addTaskActionTest))
+  .toStrictEqual([Object.assign({},story1,{taskIds:[1,2]})])
+})
+const task2=Object.assign({},addTaskActionTest.task,{id:2})
+it('addTask added Task Object in state',()=>{
+  expect(tasks([],addTaskActionTest)).toStrictEqual(
+    [task2]
+  )
+})
+
+test('update Status',()=>{
+  expect(tasks([task2],{type:'UPDATE_STATUS',taskId:2,status:TaskStatus.ACTIVE}))
+  .toStrictEqual([Object.assign({},task2,{status:'ACTIVE'})])
+
+})
+
 
 
 it('renders without crashing', () => {
